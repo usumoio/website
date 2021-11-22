b2a93edebc83a5cd7b2d14b8183251169d732c75
Michael Spivack <michael.paul.spivack@gmail.com>
2021-11-22 12:12:14 -0500

    forgot to add files on routes update

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+/**
+ * about page of the app
+ */
+import {LowerNavButton} from '../components/lower_nav_button';
+
+export const About = () => {
+
+    return (
+
+
+        <div class='main-board'>
+
+            <div class='title-section'>
+                <span>USUMOIO.COM</span>
+            </div>
+
+
+            <LowerNavButton />
+
+        </div>
+
+    );
+
+}
