5e2b17895c85c9ebd0986e0189379034a0e82216
Michael Spivack <michael.paul.spivack@gmail.com>
2021-12-03 00:21:55 -0500

    about section stable on mobile and web

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,8 +12,12 @@ export const About = () => {
             <HeaderSection title={'ABOUT'} />
 
             <div className='text-section-about'>
-                <p>Hi. I'm Mike Spivack. This is my website. Its pretty simple. I'm using it to showcase a few of my ideas.</p> 
+
+                <p>Hi. I'm Mike Spivack. This is my website. It's pretty simple. I'm using this website to showcase a few of my ideas and link to a few things I think are interesting.</p> 
                 <p>You can click around and check out the links. One of the ideas I'm checking out is website design. I'm using this website to practice that.</p>
+                <p>Oh! Here is an important note. I don't own any of the beatiful images you see in the background. All of them can be found here on the <u><a href="https://www.sgeier.net/fractals/indexe.php">artist's website</a></u>.</p>
+                <p>If you're looking for more information about me, check out the USUSMOIO Stuff section, I even include a resume there.</p>
+
             </div>
 
         </>
